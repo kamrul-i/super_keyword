9e433d0fd1eca435014f4e00b464432504f58d85
offic <offic@DESKTOP-GLF4UNU>
2024-07-07 10:15:46 +0600

    Introduction to "super" keyword.

---
+++ src/main/java/super_keyword/Super_keyword.java
@@ -0,0 +1,8 @@
+package super_keyword;
+
+public class Super_keyword {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome! Introduction to \"super keyword Demo\".");
+    }
+}
